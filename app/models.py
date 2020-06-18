ed1a5e4555ba848031ed0c91e8bac30391d379da
AIT TALEB Ilyas <ilyas.aittaleb@gmail.com>
2020-06-18 15:22:46 +0200

    checkpoint tweet unittest

---
+++ app/models.py
@@ -0,0 +1,10 @@
+# app/models.py
+
+from datetime import datetime
+
+class Tweet():
+    def __init__(self, text):
+        self.text = text
+        self.created_at = datetime.now()
+        self.id = None
+
