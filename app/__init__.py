12f3df3b91c2d48f1c1262c6cfad5167672bd92a
AIT TALEB Ilyas <ilyas.aittaleb@gmail.com>
2020-06-18 14:54:39 +0200

    New flask project boilerplate

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
-# app/__init__.py
+                       # app/__init__.py
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/hello')
-    def hello():
-        return "Hello World!"
+    from .main.controllers import main
+
+    app.register_blueprint(main)
 
     return app
